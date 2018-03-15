afa81632dc88ab47c732f4a7cd25e5c097018f4d
Ahmad Taufiq <ataufiq665@gmail.com>
2018-03-15 09:40:40 +0700

    Add ForeignKey and Seeder for data dummy

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory;
 
 class CommentsTableSeeder extends Seeder
 {
@@ -11,6 +12,23 @@ class CommentsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // reset table comments
+        DB::table('comments')->truncate();
+
+        //insert 10 dummy data to comments
+        $comments = [];
+        $faker = Factory::create();
+
+        for ($i=0; $i < 10; $i++) {
+            $date = date('Y-m-d H:i:s', strtotime("2018-03-08 11:43:00 +{$i} days"));
+            $comments[] = [
+                'post_id' => rand(1, 10),
+                'body' => $faker->sentence(rand(10, 12), true),
+                'created_at' => $date,
+                'updated_at' => $date
+            ];
+        }
+
+        DB::table('comments')->insert($comments);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,29 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // turn off foreign key checks
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+        // Reset Users Table
+        DB::table('users')->truncate();
+
+        // Insert 3 Dummy Users
+        DB::table('users')->insert([
+            [
+                'name' => 'Budi',
+                'email' => 'chandika7d@gmail.com',
+                'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'John Doe',
+                'email' => 'johndoe@gmail.com',
+                'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'Andi',
+                'email' => 'andi@gmail.com',
+                'password' => bcrypt('123456'),
+            ],
+        ]);
     }
 }
